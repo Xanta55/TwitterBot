2d04c051d133fe8549e0b10c40227cea4a83661a
Waage <daniel.waage@stud.fh-luebeck.de>
2020-03-30 22:46:35 +0200

    Initial Commit

---
+++ mainBot.py
@@ -0,0 +1,16 @@
+#USER: UEBERALLBOTS
+#@: @uberallbots
+#PW: BotsInMyPants
+#Dev-name: Piggud
+import tweepy
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+print('My Bot')
