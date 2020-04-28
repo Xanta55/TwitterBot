a5e51f14d2aef9dedb4cfabb003130a7ce0e7462
marcel.azadi <marcel.azadi@stud.th-luebeck.de>
2020-04-28 12:46:10 +0200

    Update, Mention Antworten und Search Antworten funktionieren

--- mainBot.py
+++ mainBot.py
@@ -11,6 +11,19 @@ import random
 import time
 import tweepy
 
+
+# Bitte geheim halten:
+CONSUMER_KEY = '8KksbsyJJGDquDInzOhUnLDnT'
+CONSUMER_SECRET = 'Wpadb0rR8LQeFEocJuED3Uix6UGIiQltaqIeWxCy3PvjxM5xBn'
+ACCESS_KEY = '1244695367284817922-oHRFPkSICOg1YF48ddlmXBPkEPHP5f'
+ACCESS_SECRET = 'kik1JvgMJ3zHVb9rD1FZ89yqp62XaqnlWSWbagGVLnMUT'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+FILE_NAME_ID = 'last_seen_id.txt'
+FILE_NAME_TEMPLATES = 'templates.txt'
+
 # ---------------------------------------------------------------------- #
 # Hier alles an Notizen:
 # um zu sehen, welcher typ etwas ist: type(Object)
@@ -30,14 +43,14 @@ import tweepy
 
 # Zieht sich die zuletzt gesehene ID aus der Textdatei
 def retrieve_last_seen_id(file_name):
-    f_read = open(FILE_NAME_ID, 'r')
+    f_read = open(file_name, 'r')
     last_seen_id = int(f_read.read().strip())
     f_read.close()
     return last_seen_id
 
 # Speichert die zuletzt gesehene ID in der Textdatei
 def store_last_seen_id(last_seen_id, file_name):
-    f_write = open(FILE_NAME_ID, 'w')
+    f_write = open(file_name, 'w')
     f_write.write(str(last_seen_id))
     f_write.close()
     return
@@ -101,13 +114,14 @@ def searchForTweets(api, hashtag):
 def reply_to_tweets(hashtag):
     search = searchForTweets(api, hashtag)
     nextTweet = search[0]
-    last_seen_id = retrieve_last_seen_id(FILE_NAME_ID)
-    mentions = api.mentions_timeline(last_seen_id, tweet_mode = 'extended')
+#    last_seen_id = retrieve_last_seen_id(FILE_NAME_ID)
+    mentions = api.mentions_timeline(tweet_mode = 'extended')
 
     for mention in reversed(mentions):
-        last_seen_id = mention.id
-        store_last_seen_id(last_seen_id, FILE_NAME_ID)
-        api.update_status("@" + mention.user.screen_name + "Antwort", mention.id)
+#        last_seen_id = mention.id
+#        store_last_seen_id(last_seen_id, FILE_NAME_ID)
+        api.update_status("@" + mention.user.screen_name + " Antwort", mention.id)
+        time.sleep(5)
 
 #   templates = getTemplatesFromFile("./templates.txt")
     theme = "die Wirtschaftskonjunktur der deutschan Nation"
@@ -116,18 +130,6 @@ def reply_to_tweets(hashtag):
     pass
 # ---------------------------------------------------------------------- #
 # MainLoop:
-
-# Bitte geheim halten:
-CONSUMER_KEY = '8KksbsyJJGDquDInzOhUnLDnT'
-CONSUMER_SECRET = 'Wpadb0rR8LQeFEocJuED3Uix6UGIiQltaqIeWxCy3PvjxM5xBn'
-ACCESS_KEY = '1244695367284817922-oHRFPkSICOg1YF48ddlmXBPkEPHP5f'
-ACCESS_SECRET = 'kik1JvgMJ3zHVb9rD1FZ89yqp62XaqnlWSWbagGVLnMUT'
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
-FILE_NAME_ID = 'last_seen_id.txt'
-FILE_NAME_TEMPLATES = 'templates.txt'
 
 while True:
     reply_to_tweets("#Corona")
